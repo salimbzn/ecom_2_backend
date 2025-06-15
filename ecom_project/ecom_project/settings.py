0a0b6f90652799e8c6327dfe358653448ff91162
punisherHar <devilmeliodas0@gmail.com>
2025-06-15 21:32:42 +0200

    m

--- ecom_project/ecom_project/settings.py
+++ ecom_project/ecom_project/settings.py
@@ -127,6 +127,7 @@ MEDIA_ROOT = BASE_DIR / "media"
 MEDIA_URL  = "/media/"
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
