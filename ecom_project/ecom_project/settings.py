de184a50dee08fbece4a3e8d3896027ab078a212
punisherHar <devilmeliodas0@gmail.com>
2025-06-15 22:16:46 +0200

    m

--- ecom_project/ecom_project/settings.py
+++ ecom_project/ecom_project/settings.py
@@ -87,13 +87,25 @@ WSGI_APPLICATION = 'ecom_project.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+import dj_database_url
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+DATABASE_URL = os.environ.get("DATABASE_URL")
+
+if DATABASE_URL:
+    # In production: use Postgres
+    DATABASES = {
+        "default": dj_database_url.parse(DATABASE_URL, conn_max_age=600)
     }
-}
+else:
+    # Local dev: fallback to SQLite
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
+
+
 
 
 # Password validation
