dd1cc4cf032c9e59ed65caca8e3e9ba89428092d
punisherHar <devilmeliodas0@gmail.com>
2025-06-28 21:14:59 +0200

    m

--- ecom_project/ecom_project/settings.py
+++ ecom_project/ecom_project/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["ecom-1qve.onrender.com", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["ecom-1qve.onrender.com", "localhost","ecom-shoe-b2nx.onrender.com", "127.0.0.1"]
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "https://ecom-client-iqeq.onrender.com"
