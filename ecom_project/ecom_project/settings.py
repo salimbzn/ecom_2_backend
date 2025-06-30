679f49ef1ed4cca52de00f04751471c774fcc0c9
haroun-messaoudi <131620959+haroun-messaoudi@users.noreply.github.com>
2025-06-30 17:42:56 +0200

    Update settings.py

--- ecom_project/ecom_project/settings.py
+++ ecom_project/ecom_project/settings.py
@@ -30,7 +30,7 @@ load_dotenv(BASE_DIR / ".env")
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["ecom-1qve.onrender.com", "localhost","ecom-shoe-b2nx.onrender.com", "127.0.0.1"]
 CORS_ALLOWED_ORIGINS = [
